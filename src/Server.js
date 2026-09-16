040849e58b4e0aaa3013febfab911acfc9fbe306
ayush9o <ayush2468.jnu@gmail.com>
2026-09-16 22:13:50 +0530

    updated file

--- src/Server.js
+++ src/Server.js
@@ -2,7 +2,14 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   console.log(req.method, req.url);
-
+ if (req.url === '/api/version') {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
+    return res.end('This is a version');
+  }
+   if (req.url === '/api/ping') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    return res.end('This is a ping');
+  }
   res.writeHead(200, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify({ status: 'TaskFlow API alive' }));
 });
